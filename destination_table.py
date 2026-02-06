1fb4266a813388b64bba0c0b944cc97d8d106c20
rashmib21 <rashmibarethiya2130@gmail.com>
2026-02-06 15:19:33 +0530

    Define the number of records to generate

--- destination_table.py
+++ destination_table.py
@@ -7,3 +7,10 @@ fake=Faker('en_In')
 #Datebase connection details
 conn=mysql.connector('GoHigh')
 cursor=conn.cursor()
+
+#Define the number of records to generate
+total_records=90
+
+#Loop to generate and insert data
+for i in range(total_records):
+
