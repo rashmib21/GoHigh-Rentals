0ace38593dd7f379dbb51ccb9aa45ba6647605b2
rashmib21 <rashmibarethiya2130@gmail.com>
2026-02-06 15:38:03 +0530

    Loop to generate and insert data

--- destination_table.py
+++ destination_table.py
@@ -1,5 +1,6 @@
 from faker import Faker
 import mysql.connector
+import random
 
 #Initialize Faker 
 fake=Faker('en_In')
@@ -13,4 +14,15 @@ total_records=90
 
 #Loop to generate and insert data
 for i in range(total_records):
+	destination_name=fake.city()
+	state=fake.state()
+	print(f"Destination: {destination_name}, {state}")
+	distance_km=random.randint(0, 2000)
+	print(distance_km)
+	is_active="True"
+
+	sql="""Insert into destination(destination_name, state, distance_km, is_active) values (%s, %s, %s, %s)"""
+	values=(destionation_name, state, distance_km, is_active)
+	cursor.execute(sql, values)
+	
 
