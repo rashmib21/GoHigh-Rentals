6ac992d1dea4535a42a58b270cf600838bc652dc
rashmib21 <rashmibarethiya2130@gmail.com>
2026-02-07 12:48:48 +0530

    Create Vehicle_table.py for data insertion

--- vehicle_table.py
+++ vehicle_table.py
@@ -1,1 +1,16 @@
 from faker import Faker
+import mysql.connector
+import random
+
+# Initialize faker
+fake=Faker('en_IN')
+
+# database connections
+
+conn=mysql.connector.connect(
+	host="localhost",
+	username="root",
+	password="1234",
+	database="GoHigh")
+
+cursor=conn.cursor()
