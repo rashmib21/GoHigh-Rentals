deb89592362b90dc21cce54801f68f7b56e36840
rashmib21 <rashmibarethiya2130@gmail.com>
2026-02-09 17:11:13 +0530

    booking and pricing are created

---
+++ booking_table.py
@@ -0,0 +1,44 @@
+from faker import Faker
+import mysql.connector
+import random
+from datetime import timedelta
+
+fake = Faker('en_IN')
+
+conn = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="1234",
+    database="GoHigh"
+)
+
+cursor = conn.cursor()
+
+statuses = ["Confirmed", "Completed", "Cancelled"]
+
+total_records = 180
+
+for i in range(total_records):
+
+    booking_date = fake.date_between(start_date="today", end_date="+30d")
+    travel_date = booking_date + timedelta(days=random.randint(1, 10))
+
+    booking_status = random.choice(statuses)
+    user_id = random.randint(1, 190)
+    vehicle_id = random.randint(11, 190)
+    destination_id = random.randint(1, 190)
+
+    sql = """INSERT INTO booking
+    (booking_date, travel_date, booking_status, user_id, vehicle_id, destination_id)
+    VALUES (%s, %s, %s, %s, %s, %s)"""
+
+    values = (booking_date, travel_date, booking_status, user_id, vehicle_id,destination_id)
+
+    cursor.execute(sql, values)
+
+conn.commit()
+
+print("Booking data inserted successfully!")
+
+cursor.close()
+conn.close()
--- pricing_table.py
+++ pricing_table.py
@@ -1,18 +1,66 @@
 from faker import Faker
 import mysql.connector
+import random
 
-# Initialize faker
-fake=Faker('en_IN')
+fake = Faker('en_IN')
 
-# database connections
+conn = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="1234",
+    database="GoHigh"
+)
 
-conn=mysql.connector.connect(
-	host="localhost",
-	user="root",
-	password="1234",
-	database="GoHigh")
+cursor = conn.cursor()
 
-cursor=conn.cursor()
+total_records = 10   
 
-prices = [
-()]
+cursor.execute("SELECT vehicle_id, category_id FROM vehicle")
+vehicle_map = {}
+
+for row in cursor.fetchall():
+    vehicle_map[row[0]] = row[1]
+
+cursor.execute("SELECT category_id, base_fare FROM vehicle_category")
+fare_map = {}
+
+for row in cursor.fetchall():
+    fare_map[row[0]] = row[1]
+
+# --- get bookings ---
+cursor.execute("SELECT booking_id, vehicle_id FROM booking")
+bookings = cursor.fetchall()
+
+inserted = 0
+
+for row in bookings:
+
+    if inserted >= total_records:
+        break
+
+    booking_id = row[0]
+    vehicle_id = row[1]
+
+    category_id = vehicle_map[vehicle_id]
+    base = fare_map[category_id]
+
+    tax = random.randint(10, 60)
+    discount = random.choice([0, 10, 20, 25, 50])
+    total = base + tax - discount
+
+    sql = """INSERT INTO pricing
+    (base_amount, tax_amount, discount, total_amount, pricing_type, booking_id)
+    VALUES (%s, %s, %s, %s, %s, %s)"""
+
+    values = (base, tax, discount, total, "Distance", booking_id)
+
+    cursor.execute(sql, values)
+
+    inserted += 1
+
+conn.commit()
+
+print(inserted, "records inserted successfully!")
+
+cursor.close()
+conn.close()
