a1dbe6e3fc2b7d14d4dbb4d118a584bba3e4870a
rashmib21 <rashmibarethiya2130@gmail.com>
2026-02-20 18:39:02 +0530

    Add style in register.html

--- Flask/__init__.py
+++ Flask/__init__.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request, redirect, session, flash
+from flask import Flask, render_template
 from dotenv import load_dotenv
 import mysql.connector
 
@@ -8,15 +8,14 @@ def create_app(test_config=None):
     
     app = Flask(__name__)
 
+    #  Set secret key (from .env file)
+    app.secret_key = os.getenv("SECRET_KEY")
+
     # Get values from .env
     host = os.getenv("DB_HOST")
     user = os.getenv("DB_USER")
     password = os.getenv("DB_PASSWORD")
     database = os.getenv("DB_NAME")
-
-
-    # print("Host:", host)
-    # print("User:", user)
 
     # Connect to database
     connection = mysql.connector.connect(
@@ -26,7 +25,7 @@ def create_app(test_config=None):
         database=database
     )
 
-    app.db = connection   # Attach DB to app
+    app.db = connection
 
     if test_config is None:
         app.config.from_pyfile('config.py', silent=True)
@@ -36,12 +35,16 @@ def create_app(test_config=None):
     os.makedirs(app.instance_path, exist_ok=True)
 
     @app.route('/')
-    def welcome():
+    def index():
         return render_template("index.html")
+
+    @app.route('/register', methods=['get', 'post'])
+    def register():
+        return render_template('register.html')    
 
     return app
 
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(debug=True, host='0.0.0.0', port="9000")
+    app.run(debug=True, host='0.0.0.0', port=9000)   
